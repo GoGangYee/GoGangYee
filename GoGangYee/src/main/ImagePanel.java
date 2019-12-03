cc49739b38be8f9a0d0c5e464e14a77ef48e3bde
Jimin16 <qlrwlals1234@naver.com>
2019-12-04 03:05:02 +0900

    Add ClearPanel,java and ImagePanel.java

---
+++ GoGangYee/src/main/ImagePanel.java
@@ -0,0 +1,75 @@
+package main;
+
+import java.awt.Graphics;
+import java.awt.LayoutManager;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+//배경 이미지 만들어주는 클래스. 모든 컴포넌트 및 (투명)패널을 이 패녈에 넣어주면 된다.
+public class ImagePanel extends JPanel{
+	BufferedImage img;
+	
+	public ImagePanel(){
+		try {
+			img=ImageIO.read(new File("image/sky.png"));	//default는 하늘
+		}catch(IOException e) {
+			JOptionPane.showMessageDialog(null,"이미지 불러오기 실패");
+			System.exit(0);
+		}
+	}
+	
+	public ImagePanel(LayoutManager l){
+		try {
+			img=ImageIO.read(new File("image/sky.png"));	//default는 하늘
+		}catch(IOException e) {
+			JOptionPane.showMessageDialog(null,"이미지 불러오기 실패");
+			System.exit(0);
+		}
+		this.setLayout(l);
+	}
+	
+	public void changeImage(int num) {	//사진 바꾸기 함수
+		if(num==1) {	//하늘
+			try {
+				img=ImageIO.read(new File("image/sky.png"));
+			}catch(IOException e) {
+				JOptionPane.showMessageDialog(null,"이미지 불러오기 실패");
+				System.exit(0);
+			}
+		}
+		if(num==2) {	//눈
+			try {
+				img=ImageIO.read(new File("image/snow.png"));
+			}catch(IOException e) {
+				JOptionPane.showMessageDialog(null,"이미지 불러오기 실패");
+				System.exit(0);
+			}
+		}
+		if(num==3) {	//황사
+			try {
+				img=ImageIO.read(new File("image/yellodust.png"));
+			}catch(IOException e) {
+				JOptionPane.showMessageDialog(null,"이미지 불러오기 실패");
+				System.exit(0);
+			}
+		}
+		if(num==4) {	//밤하늘
+			try {
+				img=ImageIO.read(new File("image/night.png"));
+			}catch(IOException e) {
+				JOptionPane.showMessageDialog(null,"이미지 불러오기 실패");
+				System.exit(0);
+			}
+		}
+	}
+	
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		g.drawImage(img,0,0,null);
+	}
+}
